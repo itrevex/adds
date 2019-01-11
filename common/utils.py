f9ded4184a1e8f219041bcbc9f08c7f2caab6982
=Isaac <=trevex2000@live.com>
2019-01-11 06:42:12 +0300

    create date time string

---
+++ common/utils.py
@@ -0,0 +1,35 @@
+import datetime
+
+class Utils:
+    
+    now = datetime.datetime.now()
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def dateString():
+        '''
+        Return date string 
+        190111
+        '''
+
+        return "%s%02d%d" %(str(Utils.now.year)[-2:], Utils.now.month, Utils.now.day)
+
+    @staticmethod
+    def timeString():
+        '''
+        Return time string 
+        0633
+        '''
+
+        now = datetime.datetime.now()
+        return "%02d%02d" %(Utils.now.hour, Utils.now.minute)
+
+    def dateTimeString():
+        '''
+        Return time string 
+        190111_0633
+        '''
+
+        return Utils.dateString() + "_" + Utils.timeString()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import sys
 sys.path.append("./detailing/")
+sys.path.append("./common/")
+
 from detailing import Detailing
+from utils import Utils
 
 
 if __name__ == "__main__":
-    Detailing().start()
+    # Detailing().start()
+    print (Utils.dateTimeString())
