7e462b1156c94ed99f74172dfadf075045ec75e5
=Isaac <=trevex2000@live.com>
2019-01-11 06:58:20 +0300

    show updated name

--- detailing/detailing.py
+++ detailing/detailing.py
@@ -1,12 +1,19 @@
 import ezdxf
+import sys
+
+sys.path.append("./common/")
+from utils import Utils
 
 class Detailing:
     def __init__(self):
         pass
     
-    def start(selfs):
+    def start(self):
         dwg = ezdxf.new('R2010')  # create a new DXF R2010 drawing, official DXF version name: 'AC1024'
 
         msp = dwg.modelspace()  # add new entities to the model space
         msp.add_line((0, 0), (10, 0))  # add a LINE entity
-        dwg.saveas('output\detail.dxf')
+        dwg.saveas('output\%s'%self.getFileName())
+
+    def getFileName(self):
+        return Utils.dateTimeString() +"-detail.dxf"
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import sys
 sys.path.append("./detailing/")
-sys.path.append("./common/")
 
 from detailing import Detailing
-from utils import Utils
 
 
 if __name__ == "__main__":
-    # Detailing().start()
-    print (Utils.dateTimeString())
+    Detailing().start()
