7a4686c3eca7cb3cc170f3e278a07b117535ec8c
=Isaac <=trevex2000@live.com>
2019-02-02 07:14:17 +0300

    create hatch entity object

---
+++ detailing/dxf_entities/entity_hatch.py
@@ -0,0 +1,23 @@
+from common.constants import Constants
+
+class EntityHatch:
+    '''
+    Hatch Entity
+    has type, poly_line_point and layer
+    '''
+    
+    def __init__(self, path = [], layer="0"):
+
+        self.path = path
+        self.layer = layer
+        self.type = Constants.ENTITY_HATCH
+
+    def addPoint(self, pt):
+        self.path.append = tuple(pt)
+
+    def setPath(self, path):
+        self.path = path
+
+    def setLayer(self, layer):
+        self.layer = layer
+
