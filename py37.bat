a2775f2de1d856b6cfda633865d6563941ff2505
=Isaac <=trevex2000@live.com>
2019-02-04 06:53:20 +0300

    not supposed to be tracked

--- py37.bat
+++ py37.bat
@@ -10,6 +10,6 @@ REM py -3.7-64 -m pypi main.spec
 REM py -3.7-64 hello.py
 REM py -3.7-64 -c "import site; print(site.getsitepackages())"
 REM py -3.7-64 -m PyInstaller main.spec
-py -3.7-64 main.py 
-REM py -3.7-64 main.py "input/input_data.json"
+REM py -3.7-64 main.py 
+py -3.7-64 main.py "input/input_data.json"
 
