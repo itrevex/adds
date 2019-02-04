3ffc579cf7c4114178c305c74f5f8e774e2de977
=Isaac <=trevex2000@live.com>
2019-02-04 06:50:43 +0300

    Specifiy one or two sections per span

--- detailing/beams/span.py
+++ detailing/beams/span.py
@@ -1,3 +1,4 @@
+from common.messages import Messages
 class Span:
     '''
     defines span with the span centre to centre length as span_length
@@ -9,14 +10,47 @@ class Span:
     SPAN_LENGTH = "length_m"
     SPAN_SECTION_LEFT = "section_left"
     SPAN_SECTION_RIGHT = "section_right"
+    SPAN_SECTION = "section"
 
     def __init__(self, name, props, index = 0):
         self.name = name
         self.span_length = float(props[Span.SPAN_LENGTH])
-        self.section_left = props[Span.SPAN_SECTION_LEFT]
-        self.section_right = props[Span.SPAN_SECTION_RIGHT]
+        self.section_left = self.getSectionLeft(props)
+        self.section_right = self.getSectionRight(props)
         self.index = index
-        
+    
+    def getSectionLeft(self, props):
+        section = ""
+        Messages.d(props)
+        try:
+           section = props[Span.SPAN_SECTION_LEFT]
+        except KeyError:
+            try:
+                section = props[Span.SPAN_SECTION_RIGHT]
+            except KeyError:
+                try:
+                    section = props[Span.SPAN_SECTION]
+                except KeyError:
+                    Messages.showError("Please check and make sure you specified beam sections")
+
+
+        return section
+
+    def getSectionRight(self, props):
+        section = ""
+        try:
+           section = props[Span.SPAN_SECTION_RIGHT]
+        except KeyError:
+            try:
+                section = props[Span.SPAN_SECTION_LEFT]
+            except:
+                try:
+                    section = props[Span.SPAN_SECTION]
+                except:
+                    Messages.showError("Please check and make sure you specified beam sections")
+
+
+        return section
 
     def setSpanLength(self, span_length):
         self.span_length = float(span_length)
