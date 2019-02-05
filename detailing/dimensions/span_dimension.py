d2484b1e9ee4bd1d49f15566bf0978c7ea949d9f
=Isaac <=trevex2000@live.com>
2019-02-05 06:31:26 +0300

    Refactor code

---
+++ detailing/dimensions/span_dimension.py
@@ -0,0 +1,8 @@
+class SpanDimension:
+    def __init__(self, dim_points):
+        pass
+        self.dim_points = []
+        self.dim_style =""
+        self.angle = 0.
+        self.text = ""
+        self.start_point = self.dim_points[0]
---
+++ detailing/dxf_entities/entity_dimension.py
@@ -0,0 +1,12 @@
+from common.constants import Constants
+
+class EntityDimension:
+    def __init__(self, dim_points):
+        pass
+        self.points = dim_points
+        self.dim_style =""
+        self.angle = 0.
+        self.text = ""
+        self.starting_point = self.points[0]
+        self.type = Constants.ENTITY_DIMENSION 
+        self.layer = Constants.LAYER_DIMENSIONS 
