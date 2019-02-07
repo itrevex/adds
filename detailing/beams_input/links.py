3cb3b7d6aa4519fa80ff41db24e9d164195c51c3
=Isaac <=trevex2000@live.com>
2019-02-07 06:06:06 +0300

    create links object

---
+++ detailing/beams_input/links.py
@@ -0,0 +1,51 @@
+from common.constants import Constants
+
+class SupportType:
+    '''
+    Defines A link
+    This class will be used to calculate the links labels and return
+    offset on the end depending on whether the links span length is 
+    provided or not
+
+    diameter: Strength and diameter of link
+    bar_mark: Bar mark for link
+    shape_code: Shape code for link BS 8666: 2005
+    spacing: Link spacing
+    offset: Offset from edge of support, default is 50
+    length: 2.3 
+    '''
+    DIAMETER = "diameter"
+    BAR_MARK = "bar_mark"
+    SHAPE_CODE = "shape_code"
+    SPACING = "spacing" 
+    OFFSET = "offset" 
+    LENGTH = "length"  
+
+    def __init__(self, links):
+        self.links_input = links
+        self.diameter = self.getValue(SupportType.DIAMETER)
+        self.bar_mark = self.getValue(SupportType.BAR_MARK)
+        self.shape_code = self.getValue(SupportType.SHAPE_CODE)
+        self.spacing = self.getValue(SupportType.SPACING)
+        self.offset = self.getOffset()
+        self.length = self.getLength() 
+
+    
+    def getValue(self, key):
+        return self.links_input[key]
+    
+    def getLength(self):
+        try:
+            return self.links_input(SupportType.LENGTH)
+        except KeyError:
+            return None
+        
+
+    def getOffset(self):
+        try:
+            return self.links_input(SupportType.OFFSET)
+        except KeyError:
+            return Constants.LINKS_DEFAULT_OFFSET 
+
+
+        
