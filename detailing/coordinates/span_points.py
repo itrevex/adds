6597c1b3bfc91811c89de7835f7c24158b2a5403
=Isaac <=trevex2000@live.com>
2019-02-08 05:17:50 +0300

    create super class span_points

---
+++ detailing/coordinates/span_points.py
@@ -0,0 +1,26 @@
+from .coord_change import ChoordChange
+
+class SpanPoints(ChoordChange):
+    
+    '''
+    Span coordinate give the centre to centre coordinates of the span
+    It gives coordinate of starting_point and end point
+
+     |     |                              |     |
+     |  ___|______________________________|___  |
+     |  sta|rt                            |   en|d 
+     |     |                              |     |  
+
+     start_point is at top beam_line on the left
+    '''
+    ONE_M_IN_MM = 1000.
+
+    def __init__(self, starting_point, span_length, beam_depth, scale_factor = 1):
+        
+        super(SpanPoints, self).__init__(scale_factor)
+
+        self.beam_depth = float(beam_depth)
+        self.span_length = span_length * SpanPoints.ONE_M_IN_MM
+
+        self.start_point = tuple(starting_point)
+        self.end_point = self.changeX(self.start_point, self.span_length) #covert length to mm
