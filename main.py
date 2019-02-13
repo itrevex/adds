4b6d711c98656889b6d80ffbc6f7c96261dc8f37
=Isaac <=trevex2000@live.com>
2019-02-13 07:03:37 +0300

    print empty line before logging info

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from detailing.details.all_beams import AllBeamsDetails
 from detailing.dxf import DxfDraw
 from common.load_data import LoadData
+from common.messages import Messages
 # import detailing.dimensions.trial_dim 
 
 app_data = LoadData()
 
 if __name__ == "__main__":
     # appData.getInputFilePath()
+    Messages.i("")
     all_beams = AllBeamsDetails(app_data)
     dxfDraw = DxfDraw(app_data)
     dxfDraw.drawEntities(all_beams.getAllBeamsEntities())
